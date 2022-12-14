11cc8481713633ad1cb8a8d9ea0cdb3c575b46f9
Syfer05 <syfer@gmx.fr>
2022-12-15 00:50:54 +0100

    First Test

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import os
 from urllib import request
 
+# Create directory
+dirName = input("Enter a folder name: ")
+try:
+    # Create target Directory
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Folder created ") 
+except FileExistsError:
+    print("Directory " , dirName ,  " Folder already exists")
 
+fileType = input("Enter the filetype: ")
 
-# Define the remote file to retrieve
+loopStart = input("Start of the loop:")
+loopEnd = input("End of the loop:")
+
+# Define the path for the remote files to retrieve
 remote_url = 'https://upload.wikimedia.org/wikipedia/commons/b/b5/International_Morse_Code.svg'
 
-# Define the local filename to save data
-local_file = 'File_p1.svg'
+# Define the local filenames to save data
+local_file = dirName+'/File_p1'+'.'+fileType
 
 
 
